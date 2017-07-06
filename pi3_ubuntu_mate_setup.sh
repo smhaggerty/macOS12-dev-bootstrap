7aad0061b20597386ba3dd22e02a315ac2dfa4c4
Shane Haggerty <shanehaggerty@Shanes-MacBook-Pro.local>
2017-07-05 22:04:41 -0700

    added to pi 3 setup script

--- pi3_ubuntu_mate_setup.sh
+++ pi3_ubuntu_mate_setup.sh
@@ -1,26 +1,39 @@
 #! /bin/sh
 
-echo \\apt update and upgrade...\\n;
-sudo apt update;
-sudo apt upgrade;
+echo \\napt update and upgrade ...\\n;
+sudo apt -y update;
+sudo apt -y upgrade;
 
-echo \\nInstalling vim...\\n;
-sudo apt install vim;
+echo \\nInstalling vim ...\\n;
+sudo apt -y install vim;
 
-echo \\nInstalling git...\\n;
-sudo apt install git;
+echo \\nInstalling git ...\\n;
+sudo apt -y install git;
 
-echo \\nInstalling curl...\\n;
-sudo apt install curl;
+echo \\nInstalling curl ...\\n;
+sudo apt -y install curl;
 
-echo \\nInstalling tmux\\n;
-sudo apt install tmux;
+echo \\nInstalling tmux ...\\n;
+sudo apt -y install tmux;
 
-echo \\nInstalling haskell…\\n;
-sudo apt install haskell-platform;
+echo \\nInstalling haskell ...\\n;
+sudo apt -y install haskell-platform;
 
-echo \\nInstalling chromium...\\n;
-sudo apt install chromium-browser;
+echo \\nInstalling fail2ban ...\\n;
+sudo apt -y install fail2ban;
+sudo touch /etc/fail2ban/jail.local;
+sudo tee -a /etc/fail2ban/jail.local << 'EOF'
+[ssh]
+
+enabled = true
+port = ssh
+filter = sshd
+logpath = /var/log/auth.log
+bantime = 2678400
+banaction = iptables-allports
+findtime = 604800
+maxretry = 3
+EOF
 
 echo \\nInstalling and linking dotfiles\\n;
 cd ~;
@@ -29,3 +42,6 @@ rm .vimrc;
 ln -s .dotfiles/.vimrc .vimrc;
 rm -rf .vim;
 ln -s .dotfiles/.vim .vim;
+
+echo \\nCleaning up ...\\n;
+sudo apt -y autoremove;
