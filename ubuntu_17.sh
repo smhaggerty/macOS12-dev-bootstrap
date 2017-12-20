d6854b1d154702a700b73809f94410c6b9a34e9b
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-12-19 16:31:44 -0800

    added git config info

--- ubuntu_17.sh
+++ ubuntu_17.sh
@@ -7,6 +7,9 @@ sudo apt update;
 sudo apt -y upgrade;
 sudo apt install -y vim;
 sudo apt install -y git;
+git config --global user.email "shanehaggerty@umail.ucsb.edu";
+git config --global user.name "Shane Haggerty";
+sudo apt install -y curl;
 sudo apt install -y nodejs;
 
 # Install npm and setup global install directory
@@ -23,14 +26,13 @@ sudo apt install -y python3;
 sudo apt install -y golang-go;
 
 # Install RVM
-sudo apt-get install software-properties-common;
-sudo apt-add-repository -y ppa:rael-gc/rvm;
-sudo apt-get update;
-sudo apt-get install -y rvm;
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB;
+\curl -sSL https://get.rvm.io | bash -s stable --ruby;
 
 cd ~;
 rm -rf temp;
 sudo apt purge -y ubuntu-web-launchers;
+sudo su -c usermod -aG rvm shane
 echo "";
 echo "Run command 'usermod -aG rvm <user>' as root, then check 'run command as login shell' in gnome-terminal profile preferences to finish rvm setup"
 
